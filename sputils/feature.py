1194bd6d6780680841d92a6846f7c6342192a589
Fumiali Taguchi <25476669+F-Tag@users.noreply.github.com>
2022-06-25 07:47:32 +0900

    add feature.py

---
+++ sputils/feature.py
@@ -0,0 +1,27 @@
+from lws import lws
+import numpy as np
+from librosa import feature
+
+def melspectrogram(wave, sr, frame_period=5, overlap=4, n_mels=40, fmin=70, fmax=7680, eps=1.0e-5, **kwargs):
+    fshift = int(sr * frame_period / 1000)
+    fsize = fshift * overlap
+    lws_processor=lws(fsize, fshift, **kwargs)
+
+    X = lws_processor.stft(wave)
+    A = np.abs(X)
+    M = feature.melspectrogram(S=A.T, sr=sr, n_fft=fsize, hop_length=fshift, n_mels=n_mels, fmin=fmin, fmax=fmax, htk=True)
+
+    return np.log(np.maximum(M, eps)).T
+
+
+def mel_to_audio(M, sr, frame_period=5, overlap=4, n_mels=40, fmin=70, fmax=7680, **kwargs):
+    fshift = int(sr * frame_period / 1000)
+    fsize = fshift * overlap
+    lws_processor=lws(fsize, fshift, **kwargs)
+
+    M = np.exp(M.T)
+    A = feature.inverse.mel_to_stft(M, sr=sr, n_fft=fsize, fmin=fmin, fmax=fmax, htk=True)
+    X = lws_processor.run_lws(A.T)
+    wave = lws_processor.istft(X)
+    return wave
+
